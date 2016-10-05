7852f70e72d751de3b863f148e0233c94fe17b2e
Pirolf <yuchengtu.uwm@gmail.com>
2016-10-04 22:21:20 -0700

    insert

--- pq/young_tableau.rb
+++ pq/young_tableau.rb
@@ -8,6 +8,55 @@ def down(yt, r, c)
   yt[r+1][c]
 end
 
+def left(yt, r, c)
+  return nil if c <= 0
+  yt[r][c-1]
+end
+
+def up(yt, r, c)
+  return nil if r <= 0
+  yt[r-1][c]
+end
+
+def exchange(yt, r1, c1, r2, c2)
+  temp = yt[r2][c2]
+  yt[r2][c2] = yt[r1][c1]
+  yt[r1][c1] = temp
+end
+
+def insert(yt, x)
+  r = yt.length - 1
+  c = yt[0].length - 1
+
+  yt[r][c] = x
+
+  while r >= 0 && c >= 0
+    left = left(yt, r, c)
+    up = up(yt, r, c)
+
+    curr = yt[r][c]
+
+    go_up = false
+    if left.nil?
+      return if up.nil? || curr >= up
+      go_up = true
+    else
+      if !up.nil?
+        return if curr >= up && curr >= left
+        go_up = up > left
+      end
+    end
+
+    if go_up
+      exchange(yt, r, c, r-1, c)
+      r -= 1
+    else
+      exchange(yt, r, c, r, c-1)
+      c -= 1
+    end
+  end
+end
+
 def extract_min yt
   min = yt[0][0]
   r = 0
@@ -16,11 +65,20 @@ def extract_min yt
   while r < yt.length && c < yt[0].length
     right = right(yt, r, c)
     down = down(yt, r, c)
-   
+
     go_down = false
     if right.nil?
       if down.nil?
         yt[r][c] = Float::INFINITY
+        if yt[-1] && yt[-1].first == Float::INFINITY && yt[-1].last == Float::INFINITY
+          yt.delete_at r
+        end
+
+        if yt.first && yt.first[-1] == Float::INFINITY && yt.last[-1] == Float::INFINITY
+          (0..yt.length-1).each do |x|
+            yt[x].pop
+          end
+        end
         return min
       end
       go_down = true
--- spec/pq/young_tableau_spec.rb
+++ spec/pq/young_tableau_spec.rb
@@ -9,18 +9,18 @@ RSpec.describe 'Young Tableau' do
     ]
   end
 
-  describe '#extract_min' do
+  fdescribe '#extract_min' do
     context 'when grid is 1x1' do
       it 'returns the number' do
         @yt = [[1]]
         expect(extract_min @yt).to be(1)
-        expect(@yt).to eq [[Float::INFINITY]]
+        expect(@yt).to eq []
       end
 
       it 'returns infinity' do
         @yt = [[Float::INFINITY]]
         expect(extract_min @yt).to be Float::INFINITY
-        expect(@yt).to eq [[Float::INFINITY]]
+        expect(@yt).to eq []
       end
     end
 
@@ -47,13 +47,27 @@ RSpec.describe 'Young Tableau' do
         expect(@yt).to eq [
           [3, 4, 5],
           [4, Float::INFINITY, Float::INFINITY],
-          [Float::INFINITY, Float::INFINITY, Float::INFINITY]
+        ]
+      end
+
+      it 'returns the smallest and clears a column if necessary' do
+        @yt = [
+          [1, 4, 5],
+          [6, Float::INFINITY, Float::INFINITY],
+          [7, Float::INFINITY, Float::INFINITY]
+        ]
+
+        expect(extract_min @yt).to be(1)
+        expect(@yt).to eq [
+          [4, 5],
+          [6, Float::INFINITY],
+          [7, Float::INFINITY]
         ]
       end
     end
   end
 
-  describe '#insert' do
+  fdescribe '#insert into a non full table' do
     before(:each) do
       @yt = [
         [1, 2, 4, 8],
@@ -62,13 +76,49 @@ RSpec.describe 'Young Tableau' do
       ]
     end
 
+    context 'when the table is empty' do
+      before(:each) do
+        @yt = [[Float::INFINITY]]
+      end
+
+      it 'inserts a number and maintains table' do
+        insert(@yt, 1)
+        @ut = [[1]]
+      end
+
+      it 'inserts infinity and maintains table' do
+        insert(@yt, Float::INFINITY)
+        @ut = [[Float::INFINITY]]
+      end
+    end
+
     context 'a non min' do
-      it 'maintains table' do
+      it 'inserts number and maintains table' do
         insert(@yt, 5)
         expect(@yt).to eq([
-          [1, 2, 4, 5, 8],
-          [5, 6, 6, 9, Float::INFINITY],
-          [4, 7, Float::INFINITY, Float::INFINITY, Float::INFINITY]
+          [1, 2, 4, 8],
+          [5, 5, 6, 9],
+          [4, 6, 7, Float::INFINITY]
+        ])
+      end
+
+      it 'inserts infinity and maintains table' do
+        insert(@yt, Float::INFINITY)
+        expect(@yt).to eq([
+          [1, 2, 4, 8],
+          [5, 6, 6, 9],
+          [4, 7, Float::INFINITY, Float::INFINITY]
+        ])
+      end
+    end
+
+    context 'a min' do
+      it 'maintains table' do
+        insert(@yt, 0)
+        expect(@yt).to eq([
+          [0, 2, 4, 8],
+          [1, 5, 6, 9],
+          [4, 6, 7, Float::INFINITY]
         ])
       end
     end
